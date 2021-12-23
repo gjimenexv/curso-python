b78d3216893f52affaf5cd315a947cc7f6cb4212
Guillermo Jimenez <guillejim5@gmail.com>
2021-12-22 20:50:11 -0600

    manejo de archivos

---
+++ archivos.py
@@ -0,0 +1,18 @@
+def read():
+    numbers = []
+    with open("./archivos/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+        print(numbers)
+
+def write():
+    names = ["Guillermo", "Eduardo", "Alejandro", "Gisselle"]
+    with open("./archivos/names.txt", "w", encoding="utf-8") as f:
+        for name in names:
+            f.write(name+"\n")
+
+def run():
+    write()
+
+if __name__ == '__main__':
+    run()
